2e547937325d6c93bfc842a09169c1fa212bb4ad
ZippyDev <ZippyDev@protonmail.com>
2018-09-29 13:21:19 -0600

    initial commit

---
+++ lib/coin_market_pro.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'coin_market_pro/version'
+
+module CoinMarketPro
+  # Your code goes here...
+end
---
+++ lib/coin_market_pro/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module CoinMarketPro
+  VERSION = '0.1.0'
+end
